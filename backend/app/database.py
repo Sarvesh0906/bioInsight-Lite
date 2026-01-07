a1440df295c80023f7389b47a94818996a2fe073
Sarvesh Chaurasia <123587683+Sarvesh0906@users.noreply.github.com>
2026-01-07 21:08:02 +0530

    update db

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# DATABASE_URL = "mysql+pymysql://root:saR007%40123%24@localhost:3306/chembl_36"
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(bind=engine)
+DATABASE_URL = "postgresql://bioinsight_db_user:yOtD9zNjyBCY2EsFYkkKOu4e2iM0XCmp@dpg-d5f796shg0os7380aj40-a/bioinsight_db"
 
-
-DATABASE_URL = "sqlite:///./data/chembl.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
-)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine)
